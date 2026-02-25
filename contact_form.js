d982ad2f75bc960ea603ed1b7a5958422a4b5224
Gavrila Matei <matei.gavrila@student.unitbv.ro>
2026-02-25 10:19:55 +0200

    Lab 1:HTML and CSS review

---
+++ contact_form.js
@@ -0,0 +1,9 @@
+function submitForm(){
+const nume = document.getElementById("name").value;
+const email = document.getElementById("email").value;
+const msg = document.getElementById("mesaj").value;
+console.log(nume);
+console.log(email);
+console.log(msg);
+console.warn("goodbye");
+}
