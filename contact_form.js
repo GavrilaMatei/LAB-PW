e7fd9a45321ba580855ff4d34d795e10793522ba
Gavrila Matei <matei.gavrila@student.unitbv.ro>
2026-02-25 11:32:49 +0200

    feat : salutare bazat pe ora zilei

--- contact_form.js
+++ contact_form.js
@@ -7,3 +7,22 @@ console.log(email);
 console.log(msg);
 console.warn("goodbye");
 }
+function timeBasedHello(){
+
+const date =new Date("2026-02-25 18:28");
+const dataPos =document.querySelector('header p');
+console.log(date);
+const hour =date.getHours();
+if (hour>=6 && hour<12){
+    dataPos.textContent = "Buna diminiata! Bine ai venit pe pagina mea web";
+}
+else if(hour>=12 && hour<18){
+    dataPos.textContent = "Buna ziua! Bine ai venit pe pagina mea web";
+}
+else{
+    dataPos.textContent = "Buna seara! Bine ai venit pe pagina mea web";
+}
+}
+
+
+timeBasedHello();
