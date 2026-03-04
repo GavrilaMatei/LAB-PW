dfd14ab04bf2fb598b77ab07fdd1cd97f1ea3498
Gavrila Matei <matei.gavrila@student.unitbv.ro>
2026-03-04 10:59:10 +0200

    Added feature: Validare custumizata la form

--- contact_form.js
+++ contact_form.js
@@ -1,14 +1,33 @@
-function submitForm(){
-const nume = document.getElementById("name").value;
-const email = document.getElementById("email").value;
-const msg = document.getElementById("mesaj").value;
-console.log(nume);
-console.log(email);
-console.log(msg);
-console.warn("goodbye");
+const form = document.getElementById("MSG-form");
+form.addEventListener("submit", validate);
+
+
+function validate(event){
+    const nume = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const msg = document.getElementById("mesaj").value;
+    if(nume.length<=2){
+        event.preventDefault(); 
+        document.getElementById("form-feedback").textContent = "nume prea scurt";
+        document.getElementById("form-feedback").style.color = "red";
+    }
+    else if(!email.includes("@")){
+        event.preventDefault(); 
+        document.getElementById("form-feedback").textContent = "email-ul trebuie sa contina @";
+        document.getElementById("form-feedback").style.color = "red";
+    }
+    else if(msg.length<=10){
+        event.preventDefault(); 
+        document.getElementById("form-feedback").textContent = "Mesajul trebuie sa fie de minim 10 caractere";
+        document.getElementById("form-feedback").style.color = "red";
+    }
+    else{
+        event.preventDefault(); 
+        document.getElementById("form-feedback").textContent = "Mesajul a fost trimis";
+        document.getElementById("form-feedback").style.color = "green";
+    }
 }
 function timeBasedHello(){
-
 const date =new Date();
 const dataPos =document.querySelector('header p');
 console.log(date);
@@ -23,6 +42,4 @@ else{
     dataPos.textContent = "Buna seara! Bine ai venit pe pagina mea web";
 }
 }
-
-
 timeBasedHello();
