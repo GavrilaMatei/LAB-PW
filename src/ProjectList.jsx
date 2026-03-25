d5cd09902c25b4581a0de9c94f95e02c62ee46e5
Gavrila Matei <matei.gavrila@student.unitbv.ro>
2026-03-25 10:56:24 +0200

    tratare de erorii la citirea de json

--- src/ProjectList.jsx
+++ src/ProjectList.jsx
@@ -3,6 +3,7 @@ import Card from './Card';
 function ProjectList() {
     const [projects, setProjects] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null)
         useEffect(function() {
         fetch('/data/projects.json')
             .then(function(response) {
@@ -11,17 +12,26 @@ function ProjectList() {
             .then(function(data) {
                 setProjects(data.projects);
                 setLoading(false);
-         });
+                
+         })
+            .catch(function(err){
+                setError('eroare la incarcarea datelor' + err);
+                setLoading(false);
+            });
+         
     }, []);
     if (loading) {
         return <p>Se incarca...</p>;
+    }
+    if (error){
+        return(<div>{error}</div>)
     }
     return (
         <div>
             <h3>Proiecte</h3>
             {
             projects.map(function(item, index) {
-                return <Card key={index} title={item.title} description={item.tech} />;
+                    return <Card key={index} title={item.title} description={item.tech} />;
                 })
             }
         </div>
