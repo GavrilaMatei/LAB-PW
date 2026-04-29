7bf7f458b97db9074ee2cc42bdc2d73870f4ad95
Gavrila Matei <matei.gavrila@student.unitbv.ro>
2026-04-29 10:19:21 +0300

    Post in server

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+app.use(express.json());
 // Prima ruta: raspunde la GET /
 app.get('/', function(req, res) {
  res.json({ message: 'Serverul functioneaza!' });
@@ -32,6 +33,18 @@ app.get  ('/api/stats',function(req,res){
         "not done": projects.filter(p => !p.done).length
     }
     res.json(result);
+});
+
+// POST /api/projects - adauga un proiect nou
+app.post('/api/projects', function(req, res) {
+ const newProject = {
+ id: projects.length + 1,
+ title: req.body.title,
+ tech: req.body.tech,
+ done: req.body.done || false,
+ };
+ projects.push(newProject);
+ res.status(201).json(newProject);
 });
 
 // Porneste serverul
