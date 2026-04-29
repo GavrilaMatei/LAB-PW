55755368c0fc9ed3b396341a31307c954d62c5fc
Gavrila Matei <matei.gavrila@student.unitbv.ro>
2026-04-29 11:02:54 +0300

    Get asincron + Model Project

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const Project = require('./models/Project');
 mongoose.connect('mongodb://localhost:27017/dashboard')
  .then(function() {
  console.log('Conectat la MongoDB!');
@@ -14,33 +15,14 @@ app.use(express.json());
 app.get('/', function(req, res) {
  res.json({ message: 'Serverul functioneaza!' });
 });
-// Date (temporar in memorie, vom folosi MongoDB mai tarziu)
-const projects = [
- { id: 1, title: "Pagina Personala", tech: "HTML, CSS", done: true },
- { id: 2, title: "Calculator Buget", tech: "JS", done: true },
- { id: 3, title: "Dashboard React", tech: "React", done: false },
- { id: 4, title: "API Meteo", tech: "React, API", done: false },
-];
-// GET /api/projects - returneaza toate proiectele
-app.get('/api/projects', function(req, res) {
+
+app.get('/api/projects', async function(req, res) {
+ try {
+ const projects = await Project.find();
  res.json(projects);
-});
-
-app.get  ('/api/projects/:id',function(req,res){
-    const result = projects.find(p =>
-    p.id === parseInt(req.params.id)
-    );
-    if (result) res.json(result);
-    else res.status(404).json({ error: 'Notfound' })
-});
-
-app.get  ('/api/stats',function(req,res){
-    const result = {
-        "total": projects.length,
-        "done": projects.filter(p => p.done).length,
-        "not done": projects.filter(p => !p.done).length
-    }
-    res.json(result);
+ } catch (err) {
+ res.status(500).json({ error: 'Eroare ' + err });
+ }
 });
 
 // POST /api/projects - adauga un proiect nou
